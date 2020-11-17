0b6689a55d4819e2e0f307bcf7d7b7f697ba16c6
linhaoran <linhaoran@yy.com>
2020-11-17 11:17:34 +0800

    feat: react-demo接入vw

--- react-demo/postcss.config.js
+++ react-demo/postcss.config.js
@@ -20,5 +20,16 @@ module.exports = {
       minPixelValue: 2, // 最小转换，低于 2px 不会进行转rem
       exclude: /index_ignore\.module\.scss/, // 不对匹配上的文件进行转换
     }),
+    // require('postcss-px-to-viewport')({
+    //   // 参考 https://juejin.im/post/6844903903478153224
+    //   viewportWidth: 750, // 视窗的宽度，对应的是我们设计稿的宽度
+    //   unitPrecision: 3, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
+    //   viewportUnit: 'vw', // 指定需要转换成的视窗单位，建议使用vw
+    //   // propList: ['*'], // 能转化为vw的属性列表
+    //   selectorBlackList: ['.ignore', '.hairlines'],
+    //   minPixelValue: 1,
+    //   mediaQuery: false, // 允许在媒体查询中转换`px`
+    //   exclude: [/index_ignore\.module\.scss/],
+    // }),
   ],
 }
